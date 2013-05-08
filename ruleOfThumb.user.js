28c6a69d25398e8c1523c00af2591b22f42aa4bf
beckcie fang <bluebeckie@gmail.com>
2013-05-08 22:43:17 +0800

    init commit

---
+++ ruleOfThumb.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name          rule of thumb 
+// @namespace     http://www.bluebeckie.com/
+// @description   put all the functions within the range of the thumb 
+// @include       * 
+// @require       http://code.jquery.com/jquery-2.0.0.min.js 
+// @version       1.0
+// @copyright     2013+, beckie
+// ==/UserScript==
+ 
+$(function(){
+    alert('test');
+});
